353f5138c35fd392b4a5963040414a9d6e9dd1f4
eschemel <101198267+eschemel@users.noreply.github.com>
2022-03-15 23:27:07 -0400

    Folder ReOrg and Flame Trap Changes
    - Cleaned up the folders
    - Imported Audio assets
    - Tweaked the Flame Trap mechanic

--- Assets/Scripts/EnemyStationary.cs
+++ Assets/Scripts/EnemyStationary.cs
@@ -9,52 +9,55 @@ public class EnemyStationary : Enemy
     Vector2 origCollider;
     Vector2 flameCollider;
 
-    float trapOffTime = 0.5f;
-    float trapOnTime = 6f;
-    bool flameOn = false;
+    bool flameOn;
+
+    float activeTime;
+    float activeWait;
 
     // Start is called before the first frame update
     void Start()
     {
-        changeTime = trapOffTime;
         m_Collider = GetComponent<BoxCollider2D>();
         origCollider = new Vector2(m_Collider.size.x, m_Collider.size.y);
         flameCollider = new Vector2(origCollider.x, origCollider.y * 2f);
-        timer = changeTime;
+
+        flameOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //timer countdown
-        timer -= Time.deltaTime;
-
-        if (timer < 0)
+        if (flameOn != true)
         {
-            //when timer reaches 0 then flame on... 
             StartCoroutine("TrapOn");
         }
     }
 
     IEnumerator TrapOn()
     {
+        activeTime = Random.Range(2, 6);
         //animator trigger PLUS increase box collider
-        animator.SetBool("flame", true);
         flameOn = true;
+        animator.SetBool("flame", true);
         m_Collider.size = flameCollider;
-        
+        Debug.Log("Flame On!");
+
         //wait ... seconds then end animation, bool = false, and collider back to normal
-        yield return new WaitForSeconds(trapOnTime);
-        timer = changeTime;
+        yield return new WaitForSeconds(activeTime);
+
+        activeWait = Random.Range(5, 11);
         animator.SetBool("flame", false);
-        flameOn = false;
         m_Collider.size = origCollider;
+        Debug.Log("Flame Off!");
+        yield return new WaitForSeconds(activeWait);
+        flameOn = false;
     }
 
     public override void OnCollisionEnter2D(Collision2D collision)
     {
-        if (flameOn != false) { 
-        base.OnCollisionEnter2D(collision);
+        if (flameOn != false)
+        {
+            base.OnCollisionEnter2D(collision);
             //print("Child hit with collider");
         }
     }
